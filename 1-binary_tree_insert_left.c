214317cb70ea9933f2a6d710f18cc9d3d47724ab
Duncan MacLennan <6733@holbertonstudents.com>
2023-09-06 17:14:52 -0700

    task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,6 +10,8 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new_node;
+
 	/** Check for NULL parent */
 	if (parent == NULL)
 	{
@@ -17,7 +19,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	}
 
 	/** Allocate memory for the new node */
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	new_node = malloc(sizeof(binary_tree_t));
 
 	/** Check for memory allocation failure */
 	if (new_node == NULL)
