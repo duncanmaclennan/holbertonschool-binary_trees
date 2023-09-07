e77a4ea6c0584689a8c3d79cc2285cd8767ddf33
aliciastudies <6722@holbertonstudents.com>
2023-09-06 19:18:52 -0700

    feat: func for task 8 and updated header

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - goes through binary tree using post-order traversal
+ * @tree: pointer to the root of the tree to traverse
+ * @func: pointer to func to call for each node. node value must be passed.
+ * Return: void
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
